7ca83155093d729ce0c0b3270441ba1f94a2b1d2
Tania Tabrizi <taniatabrizi98@gmail.com>
2026-07-07 16:38:28 -0700

    Update get_users.py

--- scripts/get_users.py
+++ scripts/get_users.py
@@ -1,1 +1,18 @@
+import requests
 
+def get_users(access_token):
+    url = "https://graph.microsoft.com/v1.0/users"
+
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+if __name__ == "__main__":
+    print("Microsoft Graph IAM automation module")
